9311e97703f3397890c639a0d5fbdaf8f465a2cb
Dylan Dockery <d00240561@student.dkit.ie>
2022-11-21 10:16:52 +0000

    :refactor: removed lines 48-71 in modelmesh.cs

--- 2022_3DGED_GDEngine/Core/Graphics/Meshes/ModelMesh.cs
+++ 2022_3DGED_GDEngine/Core/Graphics/Meshes/ModelMesh.cs
@@ -44,29 +44,3 @@ namespace GD.Engine
         }
     }
 }
-
-/*
- using Microsoft.Xna.Framework.Graphics;
-
-namespace GD.Engine
-{
-    public class ModelMesh : Mesh
-    {
-        private Model model;
-
-        public ModelMesh(GraphicsDevice graphicsDevice, Model model)
-            : base(graphicsDevice)
-        {
-            this.model = model;
-
-            Initialize();
-        }
-
-        protected override void CreateGeometry()
-        {
-            //extracts from the model the vertices and indices
-            model.ExtractData(ref graphicsDevice, out vertices, out indices);
-        }
-    }
-}
- */
